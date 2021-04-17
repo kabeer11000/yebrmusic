dbd10718d3500959fea3aa1b6d4e532872a831e6
kabeer11000 <52799968+kabeer11000@users.noreply.github.com>
2021-04-17 22:49:39 +0500

    Updated ./recommendation-api/requirements.txt

--- client/src/index.js
+++ client/src/index.js
@@ -57,4 +57,4 @@ ReactDOM.render(<IsTvProvider><App/></IsTvProvider>, document.getElementById("ro
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.register();
+// serviceWorker.register();
